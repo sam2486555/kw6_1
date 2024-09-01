e4f0acbff19d731f057bda5352e2b195ce12aaec
Sergey Zamuraev <zamuraev210854@gmail.com>
2024-09-01 21:59:26 +0300

    add forms.py users

---
+++ users/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from client.forms import StyleFormMixin
+from users.models import User
+
+
+class UserRegisterForm(StyleFormMixin, UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("email", "password1", "password2")
+
+
+class UserUpdateModeratorForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('is_active',)
+
+
+class UserProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password'].widget = forms.HiddenInput()
