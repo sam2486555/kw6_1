580e574c15010739a903c2613b713f21d80dfa61
Sergey Zamuraev <zamuraev210854@gmail.com>
2024-09-01 21:58:22 +0300

    create users app

---
+++ users/views.py
@@ -0,0 +1,63 @@
+import secrets
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, ListView, UpdateView
+
+from config.settings import EMAIL_HOST_USER
+from users.forms import UserRegisterForm, UserUpdateModeratorForm, UserProfileForm
+from users.models import User
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        token = secrets.token_hex(16)
+        user.token = token
+        user.save()
+        host = self.request.get_host()
+        url = f'http://{host}/users/email-confirm/{token}/'
+        send_mail(
+            subject='Подтверждение почты',
+            message=f'Для подтверждения аккаунта перейдите по ссылке: {url}',
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email],
+        )
+        return super().form_valid(form)
+
+
+def email_verification(request, token):
+    user = get_object_or_404(User, token=token)
+    user.is_active = True
+    user.save()
+    return redirect(reverse('users:login'))
+
+
+class UserListView(ListView):
+    model = User
+    template_name = 'users/user_list.html'
+
+
+class UserModeratorView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = UserUpdateModeratorForm
+    success_url = reverse_lazy('users:user_list')
+
+
+class ProfileView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = UserProfileForm
+    template_name = 'users/user_form.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def get_success_url(self):
+        return reverse_lazy('users:profile', kwargs={'pk': self.request.user.pk})
